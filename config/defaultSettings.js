7c3678e6897e002e282856cf3eea3c5a1c836a44
ccyccyccy222 <1092098326@qq.com>
2021-08-10 08:59:38 +0800

    Initial commit

---
+++ config/defaultSettings.js
@@ -0,0 +1,16 @@
+const Settings = {
+  navTheme: 'light',
+  // 拂晓蓝
+  primaryColor: '#1890ff',
+  layout: 'mix',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  fixSiderbar: true,
+  colorWeak: false,
+  title: 'Hefu-Noodle',
+  pwa: false,
+  // logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+  logo: 'https://i.loli.net/2021/08/10/bnMRFkJ1xA3Coji.png',
+  iconfontUrl: '',
+};
+export default Settings;
